b0fc7dc44cab069d7e9e368363ead1ae69c1bb80
Joshkinz <jdeastin02@gmail.com>
2017-06-23 16:37:50 -0500

    first working prototype

--- internetcheckbot.py
+++ internetcheckbot.py
@@ -1,23 +1,25 @@
 import urllib.request
 import time
+import twitter
 
 def internet_on():
 	try:
-		urllib.request.urlopen('https://172.217.8.206', timeout=1)
-		print("True")
+		urllib.request.urlopen('http://172.217.8.206', timeout=1)
+		return True
 	except urllib.request.URLError as err:
-		print("False")
+		return False
 		
 while True:
 	a = internet_on()
 	minute = 1
 	if a == False:
 		b = internet_on()
-		if b == False:
+		while b == False:
 			print("Internet offline for %i minutes." % minute)
-			time.sleep(1)
+			time.sleep(60)
 			minute = minute + 1
 		if b == True:
 			print("Internet was out for %i minutes." % minute)
 	if a == True:
-		print("Internet is up.")
+		print("Internet is up at %i:%i:%i." % ())
+		time.sleep(60)
