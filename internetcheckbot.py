7c85e6fba925e54daf2c8c400388e00808a13dc1
Joshkinz <jdeastin02@gmail.com>
2017-06-24 18:57:01 -0500

    Update internetcheckbot.py
    Made the bot check 15 times over 3 different websites for internet connection, changed the tweet slightly

--- internetcheckbot.py
+++ internetcheckbot.py
@@ -10,30 +10,55 @@ api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', acce
 #--Defines the check for internet
 
 def internet_on():
-	try:
-		urllib.request.urlopen('http://172.217.8.206', timeout=1)
-		#172.217.8.206 is google's IP
-		return True
-	except urllib.request.URLError as err:
-		return False
+	attempts = 5
+	while attempts > 0:
+		try:
+			urllib.request.urlopen('http://172.217.8.206', timeout=5)
+			#172.217.8.206 is google.com's IP (as of 6/24/2017)
+			return True
+			break
+		except urllib.request.URLError as err:
+			attempts = attempts -1
+	if attempts = 0:
+		attempts2 = 5
+		while attempts2 > 0:
+			try:
+				urllib.request.urlopen('http://68.66.66.193', timeout=5)
+				#68.66.66.193 is mediacomcable.com's IP (as of 6/24/2017)
+				return True
+				break
+			except urllib.request.URLError as err:
+				attempts2 = attempts2 - 1
+		if attempts2 = 0:
+			attempts3 = 5
+			while attempts3 > 0:
+				try:
+					urllib.request.urlopen('http://151.101.1.140', timeout=5)
+					#151.101.1.140 is reddit.com's IP (as of 6/24/2017)
+					return True
+					break
+				except urllib.request.URLError as err:
+					attempts3 = attempts3 - 1
+			if attempts3 = 0:
+				return False
 
 #--Checks if internet is up, tweets if not
 
 while True:
 	a = internet_on()
-	minute = 1
+	minute = 0
 	while a == False:
 		b = internet_on()
 		if b == False:
 			print("Internet offline for %i minutes at %s." % (minute, datetime.datetime.now()))
-			time.sleep(60)
-			minute = minute + 1
+			time.sleep(120)
+			minute = minute + 2
 		if b == True:
 			print("Internet was out for %i minutes, back up at %s." % (minute, datetime.datetime.now()))
 			if minute > 2:
-				api.PostUpdate("@MediacomSupport My internet was out for %i minutes. We've now had 3 technicians out with no fix. (This is a bot written by @Joshkinz_ )" % minute)
+				api.PostUpdate("Hey @MediacomSupport my internet was out for %i minutes. It has been going out for several minutes on a daily basis #mediacom #internet" % minute)
 			a = True
-			time.sleep(60)
+			time.sleep(120)
 	if a == True:
 		print("Internet is up at %s." % datetime.datetime.now())
-		time.sleep(60)
+		time.sleep(120)
