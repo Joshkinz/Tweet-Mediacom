73e6811f59cbd7263a01db17b8a4e595ea65f594
Volcyy <Johannes.christ@gmail.com>
2017-06-25 18:22:53 +0200

    Select random website to ping, move attempts var
    Instead of opening Google every time, a random website of the three IP
    addresses listed at the start of the file (put into a list shortly
    after)  is now selected to ping. Additionally, I moved the maximum
    attempts to a "constant" which I elaborated earlier.

--- internetcheckbot.py
+++ internetcheckbot.py
@@ -2,11 +2,16 @@ import urllib.request
 import time
 import twitter
 import datetime
+import random
 
 # Last updated 6/24/2017
 GOOGLE_IP = 'http://172.217.8.206'
 MEDIACOMCABLE_IP = 'http://68.66.66.193'
 REDDIT_IP = 'http://151.101.1.140'
+IP_ADDRESSES = [GOOGLE_IP, MEDIACOMCABLE_IP, REDDIT_IP]
+
+# The maximum attempts to ping the above IP adresses before declaring internet as off
+MAXIMUM_ATTEMPTS = 15
 
 #--Connect to Twitter
 
@@ -15,9 +20,9 @@ api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', acce
 #--Defines the check for internet
 
 def internet_on():
-	for _ in range(15):
+	for _ in range(MAXIMUM_ATTEMPTS):
 		try:
-			urllib.request.urlopen(GOOGLE_IP, timeout=5)
+			urllib.request.urlopen(random.choice(IP_ADDRESSES), timeout=5)
 			return True
 		except urllib.request.URLError:
 			pass
