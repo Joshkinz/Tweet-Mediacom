4ab36d788408d60db2f1ae31cfe57d27006c2ae9
Joshkinz <jdeastin02@gmail.com>
2017-06-25 14:13:32 -0500

    Update internetcheckbot.py
    changed mediacom ip to microsoft ip

--- internetcheckbot.py
+++ internetcheckbot.py
@@ -9,26 +9,24 @@ import twitter
 
 # Last updated 6/24/2017
 GOOGLE_IP = 'http://172.217.8.206'
-MEDIACOMCABLE_IP = 'http://68.66.66.193'
+MICROSOFT_IP = 'http://104.40.211.35'
 REDDIT_IP = 'http://151.101.1.140'
-IP_ADDRESSES = [GOOGLE_IP, MEDIACOMCABLE_IP, REDDIT_IP]
+IP_ADDRESSES = [GOOGLE_IP, MICROSOFT_IP, REDDIT_IP]
 
 # The maximum attempts to ping the above IP adresses before declaring internet as off
 MAXIMUM_ATTEMPTS = 15
-
 
 # Get login info from config
 with open('config.json') as file:
     config = json.load(file)
 
-#--Connect to Twitter
+# Connect to Twitter
 api = twitter.Api(consumer_key=config['consumer-key'],
 	consumer_secret=config['consumer-secret'],
 	access_token_key=config['access-token']['key'],
 	access_token_secret=config['access-token']['secret'])
 
-#--Defines the check for internet
-
+# Defines the check for internet
 def internet_on():
 	for _ in range(MAXIMUM_ATTEMPTS):
 		try:
@@ -38,8 +36,7 @@ def internet_on():
 			pass
 	return False
 
-#--Checks if internet is up, tweets if not
-
+# Checks if internet is up, tweets if not
 while True:
 	currently_online = internet_on()
 	minute = 0
